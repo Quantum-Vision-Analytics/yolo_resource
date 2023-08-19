88325990aadbb69fcb35cfdeb32c01ae7bb33ec8
muhammet pakyurek <m.pakyurek.vendor@intellisee.com>
2023-08-19 06:54:21 -0500

    feat: initDisplayUI is created to handle displays of image and annotations feat: each display is labeled what is inside

--- QVA_GUI/qt_gui_elements.py
+++ QVA_GUI/qt_gui_elements.py
@@ -23,17 +23,33 @@ class QtGuiElements(QWidget):
         # Parametrelerini seçme ve Algılama
         self.detect_button = QPushButton('Detection', self)
         self.initUI()
+    def initDisplayUI(self):
+        # Create a label for the image title
+        image_disp_label = QLabel("Image Display")
+        detection_disp_label = QLabel("Detection Display")
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(image_disp_label)
+        vbox.addWidget(self.org_img_display)
+        vbox2 = QVBoxLayout()
+        vbox2.addWidget(detection_disp_label)
+        vbox2.addWidget(self.detection_display)
+        hbox1 = QHBoxLayout()
+        hbox1.addLayout(vbox)
+        hbox1.addLayout(vbox2)
+        return hbox1
+
     def initUI(self):
+        hbox1 = self.initDisplayUI()
         vbox1 = QVBoxLayout()
         vbox2 = QVBoxLayout()
-        hbox1 = QHBoxLayout()
+
         hbox2 = QHBoxLayout()
         hbox3 = QHBoxLayout()
         hbox4 = QHBoxLayout()
 
         # Resim seçimi için QLabel ve QPushButton
-        hbox1.addWidget(self.org_img_display)
-        hbox1.addWidget(self.detection_display)
+
         vbox1.addWidget(self.choose_image_button)
         vbox1.addWidget(self.close_project_button)
 
